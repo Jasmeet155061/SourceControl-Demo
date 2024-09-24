a34fac99ea967508da48df3f55fbb4afa1099f53
Jasmeet Singh <jasmeetsingh155061@gmail.com>
2024-09-24 19:06:14 +0530

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Jasmeet Singh on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
